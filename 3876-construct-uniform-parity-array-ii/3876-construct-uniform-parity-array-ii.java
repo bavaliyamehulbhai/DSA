2f647124e1e594394b26960ea409093caa6d10f9
Mehul Bavaliya <163331684+bavaliyamehulbhai@users.noreply.github.com>
2026-09-03 20:40:09 +0530

    Time: 5 ms (83.33%), Space: 121.7 MB (62.32%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,36 +1,13 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        Arrays.sort(nums1); 
-        boolean allOdd = true, allEven = true;
+        int minimum = Integer.MAX_VALUE;
+        boolean allEven = true;
         for (int num : nums1) {
-            if (num % 2 == 0) allOdd = false;
-            else allEven = false;
-        }
-        if (allOdd || allEven) return true;
-        int smallestOdd = Integer.MAX_VALUE;
-        int smallestEven = Integer.MAX_VALUE;
-        for (int num : nums1) {
-            if (num % 2 == 0) smallestEven = Math.min(smallestEven, num);
-            else smallestOdd = Math.min(smallestOdd, num);
-        }
-        boolean canAllOdd = true;
-        for (int num : nums1) {
-            if (num % 2 == 0) { 
-                if (num - smallestOdd < 1 || (num - smallestOdd) % 2 == 0) {
-                    canAllOdd = false;
-                    break;
-                }
+            minimum = Math.min(minimum, num);
+            if (num % 2 == 1) {
+                allEven = false;
             }
         }
-        boolean canAllEven = true;
-        for (int num : nums1) {
-            if (num % 2 == 1) { 
-                if (num - smallestEven < 1 || (num - smallestEven) % 2 == 1) {
-                    canAllEven = false;
-                    break;
-                }
-            }
-        }
-        return canAllOdd || canAllEven;
+        return allEven || minimum % 2 == 1;
     }
 }
