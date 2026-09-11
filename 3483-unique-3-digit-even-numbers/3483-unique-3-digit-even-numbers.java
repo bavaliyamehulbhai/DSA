5760b357c4e36b5fc00354de5b18643fb5e44c06
Mehul Bavaliya <163331684+bavaliyamehulbhai@users.noreply.github.com>
2026-09-11 11:56:41 +0530

    Time: 88 ms (5.07%), Space: 46.6 MB (26.13%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,27 +1,22 @@
-public class Solution {
+class Solution {
     public int totalNumbers(int[] digits) {
-        int[] freq = new int[10];
-        for (int d : digits) freq[d]++;
-
-        int count = 0;
-        for (int num = 100; num <= 999; num++) {
-            if (num % 2 != 0) continue; 
-            int a = num / 100;
-            int b = (num / 10) % 10;
-            int c = num % 10;
-
-            int[] need = new int[10];
-            need[a]++; need[b]++; need[c]++;
-
-            boolean ok = true;
-            for (int d = 0; d < 10; d++) {
-                if (need[d] > freq[d]) {
-                    ok = false;
-                    break;
+        HashSet<Integer> map = new HashSet<>();
+        int n = digits.length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (i != j && i != k && j != k) {
+                        if (digits[i] != 0) {
+                            if (digits[k] % 2 == 0) {
+                                int number = digits[i] * 100 + digits[j] * 10 + digits[k];
+                                map.add(number);
+                            }
+                        }
+                    }
                 }
             }
-            if (ok) count++;
         }
-        return count;
+        return map.size();
     }
 }
